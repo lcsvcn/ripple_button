be3a1e16cc7b0c2c45a87d2b53b2d4aea801645b
Lucas <lcsvcn@users.noreply.github.com>
2021-05-09 20:59:45 -0300

    added dart comments

--- lib/models/ripple_button_style.dart
+++ lib/models/ripple_button_style.dart
@@ -41,7 +41,13 @@ class RippleButtonStyle {
 }
 
 class RippleButtonBorder {
+  /// [side] expects a [BorderSide]
+  /// Use this to add a border to the ripple button
   final BorderSide side;
+
+  /// [side] expects a [BorderRadius]
+  /// Use this to customize ripple button corners
+  /// This can be used to create rounded button corners
   final BorderRadius radius;
 
   const RippleButtonBorder({
